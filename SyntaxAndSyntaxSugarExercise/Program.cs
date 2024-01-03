ca691967cc28e87e4ef6059d895c13e775ab7e06
Zack Moyal <zack.moyal@gmail.com>
2024-01-02 22:17:28 -0800

    Snytax Sugar Exercise Windows

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
         }
     }
 }
